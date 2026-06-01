58e7e28253f7bf3a394e8bc606252a0adfee8326
rapidbunny1214 <danishhps17@gmail.com>
2026-06-02 01:10:08 +0500

    fix: use database url from environment

--- app/database.py
+++ app/database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/devopsdb"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:postgres@db:5432/devopsdb"
+)
 
 engine = create_engine(DATABASE_URL)
 
@@ -12,4 +16,4 @@ SessionLocal = sessionmaker(
     bind=engine
 )
 
-Base = declarative_base()
+
